b39f8ac3cc1d04033a3cda39e093a449228dbdfc
ianic <igor.anic@gmail.com>
2013-10-02 17:20:27 +0200

    Updating tests.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source "http://rubygems.org"
 
 # Specify your gem's dependencies in activerecord-sqlserver-adapter-mirroring.gemspec
+gem "rake"
+gem "tiny_tds"
 gemspec
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,36 +14,36 @@ config = YAML.load(File.read(File.join(File.dirname(__FILE__), "database.yml")))
 #ActiveRecord::Base.establish_connection(config)
 
 #define model
-class Programmer < ActiveRecord::Base 
-end                
+class Programmer < ActiveRecord::Base
+end
 
 Programmer.establish_connection(config)
 
 def setup_db
   ActiveRecord::Schema.define do
-    
+
     Programmer.connection.create_table :programmers, :force => true do |t|
       t.column :first_name, :string
       t.column :last_name, :string
     end
-    
-  end  
-end     
 
-def db_failover
-  Programmer.connection.execute("use master; ALTER DATABASE activerecord_unittest_mirroring SET PARTNER FAILOVER") 
+  end
 end
 
-def failover                    
+def db_failover
+  Programmer.connection.execute("use master; ALTER DATABASE activerecord_unittest_mirroring SET PARTNER FAILOVER")
+end
+
+def failover
   begin
     db_failover
-  rescue 
+  rescue
     sleep 1
     retry
   end
   Programmer.connection.reconnect!
   print_current_server_name
-end         
+end
 
 def print_current_server_name
   print "connected to #{Programmer.server_name}\n"
